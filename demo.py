0307fd4220ec97218878880a8d1d266261a9664e
yuhcee <egbouchenna001@gmail.com>
2022-05-19 19:25:50 +0100

    connect to db with psycopg2

---
+++ demo.py
@@ -0,0 +1,28 @@
+import psycopg2
+
+# open a connection to database
+connection = psycopg2.connect("dbname=example")
+
+# open cursor to perform database operations
+cursor = connection.cursor()
+
+# drop any existing todos table
+cursor.execute('DROP TABLE IF EXISTS todos');
+
+# (re)create the todos table
+# (note: triple quotes allow multiline text in python)
+cursor.execute('''
+CREATE TABLE todos (
+    id INTEGER PRIMARY KEY,
+    description VARCHAR NOT NULL,
+    completed BOOLEAN NOT NULL DEFAULT False
+);
+''')
+
+cursor.execute('''INSERT INTO todos (id, description, completed) VALUES (1, 'Learn Python', true);''')
+cursor.execute('''INSERT INTO todos (id, description, completed) VALUES (2, 'Go to bed', false);''')
+
+# commit, so it does the executions on the db and persists in the db
+connection.commit()
+cursor.close()
+connection.close()
