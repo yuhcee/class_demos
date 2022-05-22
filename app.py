dee7a43ffc0bb1890781717b195bda207a3e57f9
yuhcee <egbouchenna001@gmail.com>
2022-05-23 00:04:14 +0100

    add Todo object

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://yuhcee@127.0.0.1:5432/exam
 
 db = SQLAlchemy(app)
 
+class Todo():
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key = True)
+    description = db.Column(db.String(),nullable=False)
+    completed = db.Column(db.Boolean, nullable=False)
+
 @app.route('/')
 def index():
     return "Hello World"
