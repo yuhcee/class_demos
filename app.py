0a6287fbaff8afa401ddc3c33de5936c7f0d05a1
yuhcee <egbouchenna001@gmail.com>
2022-05-22 23:33:28 +0100

    launch app

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,4 +8,4 @@ def index():
 
 if __name__ == '__main__':
         app.debug = True
-        app.run(host='0.0.0.0', port=3000)
+        app.run(host='0.0.0.0', port=5000)
