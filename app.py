d5eefcc6fc8bf7d24300c2b6704b0b72682bcc7f
yuhcee <egbouchenna001@gmail.com>
2022-05-22 23:55:24 +0100

    connect db

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://yuhcee@127.0.0.1:5432/example'
+
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
