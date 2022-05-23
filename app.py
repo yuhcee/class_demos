bdad5fab1da0ce7e5f4cec26288d558daf11fcd2
yuhcee <egbouchenna001@gmail.com>
2022-05-23 11:47:57 +0100

    add Person class

--- app.py
+++ app.py
@@ -3,18 +3,30 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://yuhcee@127.0.0.1:5432/example'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-class Todo():
+class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key = True)
     description = db.Column(db.String(),nullable=False)
     completed = db.Column(db.Boolean, nullable=False)
 
+class Person(db.Model):
+    __tablename__ = 'persons'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+     return f'<Person ID: {self.id}, name: {self.name}>'
+
+db.create_all()
+
 @app.route('/')
 def index():
-    return "Hello World"
+    person = Person.query.first()
+    return "Hello! " + person.name
 
 if __name__ == '__main__':
         app.debug = True
