3479e5b02a12070da2d6268edab09a67e1533817
yuhcee <egbouchenna001@gmail.com>
2022-05-27 11:12:45 +0100

    modify app

--- app.py
+++ app.py
@@ -1,21 +1,11 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://yuhcee@127.0.0.1:5432/example'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-
-migrate = Migrate(app, db)
-
-class Todo(db.Model):
-    __tablename__ = 'todos'
-    id = db.Column(db.Integer, primary_key = True)
-    description = db.Column(db.String(),nullable=False)
-    completed = db.Column(db.Boolean, nullable=False)
-
 class Person(db.Model):
     __tablename__ = 'persons'
     id = db.Column(db.Integer, primary_key = True)
