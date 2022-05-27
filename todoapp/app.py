8bf24c6e0ef5bdb2eb0224a51540b359e4e9b41d
yuhcee <egbouchenna001@gmail.com>
2022-05-27 12:29:22 +0100

    add create route

--- todoapp/app.py
+++ todoapp/app.py
@@ -1,9 +1,10 @@
+from email.policy import default
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://yuhcee@127.0.0.1:5432/example'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://yuhcee@127.0.0.1:5432/todoapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -14,18 +15,25 @@ class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key = True)
     description = db.Column(db.String(),nullable=False)
-    # completed = db.Column(db.Boolean, nullable=False)
+    # completed = db.Column(db.Boolean, nullable=False, default=False)
     
     def __repr__(self):
      return f'<Todo ID: {self.id}, description: {self.name}>'
 
-db.create_all()
+# db.create_all()
 
 @app.route('/')
 def index():
     todo = Todo.query.first()
-    return "Your todo number" + todo.id + " is "  + todo.description
+    return f"Your todo number {todo.id} is to {todo.description}."
+
+@app.route('/todos/create')
+def create_todo():
+        description=request.data.get('description', '')
+        todo=Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
 
 if __name__ == '__main__':
         app.debug = True
-        app.run(host='0.0.0.0', port=5000)
+        app.run(host='0.0.0.0', port=4000)
