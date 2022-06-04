2dc06289b507c1063bbafd57f4e4c83a99e20b02
yuhcee <egbouchenna001@gmail.com>
2022-06-04 21:16:43 +0100

    refactor(app.py): fix route ordering

--- todoapp/app.py
+++ todoapp/app.py
@@ -91,18 +91,19 @@ def delete_todo(todo_id):
         else:
            return jsonify({'success': True})
 
-@app.route('/lists/<list_id>')
-def get_list_todos(list_id):
-  return render_template('index.html',
-  lists=TodoList.query.all(),
-  active_list=TodoList.query.get(list_id),
-  todos=Todo.query.filter_by(list_id=list_id).order_by('id').all()
-)
 
 @app.route('/')
 def index():
   return redirect(url_for('get_list_todos', list_id=1))
 
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+
+  lists=TodoList.query.all(),
+  active_list=TodoList.query.get(list_id),
+  todos=Todo.query.filter_by(list_id=list_id).order_by('id').all()
+    
+  return render_template('index.html', todos=todos, lists=lists, active_list=active_list)
 
 if __name__ == '__main__':
         app.debug = True
